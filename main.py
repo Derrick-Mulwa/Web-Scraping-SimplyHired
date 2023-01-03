5d07cff54b4c07358d916aa57d136bbcce787567
Derrick <mbalukaderrik@gmail.com>
2023-01-03 17:34:43 +0300

    Recognize previous scrapped records

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from tqdm import tqdm
+from termcolor import colored
 
 
 # I have created important functions first, and arranged the inthe right order at the bottom part of this code
@@ -196,71 +197,148 @@ benefits_list = []
 qualifications_list = []
 job_description_list = []
 
+# Read the csv with previously scraped jobs
+existing_data_dataframe = pd.read_csv("Collected data.csv", index_col="Unnamed: 0")
+
+# Get the last 3 records from the previous scrapped data. This will be used to determine when we are at par since last
+# scraping time
+match_existing = existing_data_dataframe.loc[:,
+                 ['job_title', 'company_name', 'company_location(city)', 'company_location_state', \
+                  'salary_type', 'payment_cycle', 'currency', 'job_type']].iloc[-3:, ].values
+
+match_existing_list = [list(match_existing[0]), list(match_existing[1]), list(match_existing[2])]
+
 # Combine all the functions in the most perfect order
 
-pages_to_scrape = 50
-
+page = 1
+searched_jobs = 0
 print("Fetching requested data: ")
-for i in tqdm(range(1, (pages_to_scrape + 1))):
+updated = False
+break_even = 0
+instance1 = True
 
-    # The url will change every time it loops because i will change. i reresents page number
+while updated is False:
+    if page < 70:
 
-    url = f"https://www.simplyhired.com/search?q=data+analyst&sb=dd&pn={i}"
+        # The url will change every time it loops because i will change. i reresents page number
 
-    # Get the page's contents using requests() and beautifulSoup()
+        url = f"https://www.simplyhired.com/search?q=data+analyst&sb=dd&pn={page}"
 
-    response = requests.get(url).content
+        # Get the page's contents using requests() and beautifulSoup()
 
-    soup = BeautifulSoup(response, "lxml")
+        response = requests.get(url).content
 
-    # The jobs are ordered as list items in an unordered list.The unordered list belong to class "jobs"
-    # The job details are in <article> of class "SerpJob"
+        soup = BeautifulSoup(response, "lxml")
 
-    unordered_list = soup.find("ul", class_="jobs")
-    articles = unordered_list.find_all("article", class_="SerpJob")
+        # The jobs are ordered as list items in an unordered list.The unordered list belong to class "jobs"
+        # The job details are in <article> of class "SerpJob"
 
-    for article in articles:
-        to_job_link = article.find("a", class_="SerpJob-link card-link")["href"]
-        full_link = f"https://www.simplyhired.com{to_job_link}"
+        unordered_list = soup.find("ul", class_="jobs")
+        articles = unordered_list.find_all("article", class_="SerpJob")
 
-        content = BeautifulSoup(requests.get(full_link).content, "html.parser")
-        page_information = content.find("main", class_="row")
+        for article in articles:
+            to_job_link = article.find("a", class_="SerpJob-link card-link")["href"]
+            full_link = f"https://www.simplyhired.com{to_job_link}"
 
-        job_title = get_jobTitle()
-        company_name = get_companyName_and_Rating()[0]
-        rating = get_companyName_and_Rating()[1]
-        location = get_jobLocation()
-        company_location_city = location[0]
-        company_location_state = location[1]
-        salary_details = get_jobSalary()
-        salary_type, payment_cycle, lBound, uBound, currency = salary_details[0], salary_details[1], salary_details[2], \
-                                                               salary_details[3], salary_details[4]
-        job_type = get_jobType()
-        job_benefits = get_jobBenefits()
-        qualifications = get_jobQualifications()
-        job_description = get_full_job_description()
+            content = BeautifulSoup(requests.get(full_link).content, "html.parser")
+            page_information = content.find("main", class_="row")
 
-        job_title_list.append(job_title)
-        company_name_list.append(company_name)
-        rating_list.append(rating)
-        company_location_city_list.append(company_location_city)
-        company_location_state_list.append(company_location_state)
-        salary_type_list.append(salary_type)
-        payment_cycle_list.append(payment_cycle)
-        lBound_list.append(lBound)
-        uBound_list.append(uBound)
-        currency_list.append(currency)
-        job_type_list.append(job_type)
-        benefits_list.append(job_benefits)
-        qualifications_list.append(qualifications)
-        job_description_list.append(job_description)
+            job_title = get_jobTitle()
+            company_name = get_companyName_and_Rating()[0]
+            rating = get_companyName_and_Rating()[1]
+            location = get_jobLocation()
+            company_location_city = location[0]
+            company_location_state = location[1]
+            salary_details = get_jobSalary()
+            salary_type, payment_cycle, lBound, uBound, currency = salary_details[0], salary_details[1], salary_details[2], \
+                                                                   salary_details[3], salary_details[4]
+            job_type = get_jobType()
+            job_benefits = get_jobBenefits()
+            qualifications = get_jobQualifications()
+            job_description = get_full_job_description()
 
-print(f"Scraping {pages_to_scrape} pages: Done")
+            job_title_list.append(job_title)
+            company_name_list.append(company_name)
+            rating_list.append(rating)
+            company_location_city_list.append(company_location_city)
+            company_location_state_list.append(company_location_state)
+            salary_type_list.append(salary_type)
+            payment_cycle_list.append(payment_cycle)
+            lBound_list.append(lBound)
+            uBound_list.append(uBound)
+            currency_list.append(currency)
+            job_type_list.append(job_type)
+            benefits_list.append(job_benefits)
+            qualifications_list.append(qualifications)
+            job_description_list.append(job_description)
+
+            searched_jobs += 1
+
+            # This part is for comparing and identifying jobs entered in the previous search and know when to stop to avoid duplication
+            #         and get all new updates
+
+            this_job_list = [job_title, company_name, company_location_city, company_location_state, salary_type,
+                             payment_cycle, currency, job_type]
+
+            last_searched_job = ['', '', '', '', '', '', '', '', '', '', '', '']
+            second_last_searched_job = ['', '', '', '', '', '', '', '', '', '', '', '']
+            third_last_searched_job = ['', '', '', '', '', '', '', '', '', '', '', '']
+
+            if searched_jobs == 1:
+                last_searched_job = this_job_list
+
+            elif searched_jobs == 2:
+                second_last_searched_job = last_searched_job
+                last_searched_job = this_job_list
+
+
+            elif searched_jobs == 3:
+                third_last_searched_job = second_last_searched_job
+                second_last_searched_job = last_searched_job
+                last_searched_job = this_job_list
+            else:
+                third_last_searched_job = second_last_searched_job
+                second_last_searched_job = last_searched_job
+                last_searched_job = this_job_list
+
+            match_new_jobs_list = [third_last_searched_job, second_last_searched_job, last_searched_job]
+
+            if (match_existing_list[2] == match_new_jobs_list[2]) or (match_existing_list[2] == match_new_jobs_list[1]) or\
+               (match_existing_list[2] == match_new_jobs_list[0]) or (match_existing_list[1] == match_new_jobs_list[2]) or\
+               (match_existing_list[1] == match_new_jobs_list[1]) or (match_existing_list[1] == match_new_jobs_list[0]) or\
+               (match_existing_list[0] == match_new_jobs_list[2]) or (match_existing_list[0] == match_new_jobs_list[1]) or\
+               (match_existing_list[0] == match_new_jobs_list[0]):
+                updated = True
+                if instance1 is True:
+                    break_even = searched_jobs - 1
+                    instance1 = False
+
+
+            print(colored(f"\rScanning page {page} of the website! Sucessfully scraped {searched_jobs} jobs!", "green"), end='', flush=True)
+
+        page += 1
+
+job_title_list = job_title_list[:break_even]
+company_name_list = company_name_list[:break_even]
+rating_list = rating_list[:break_even]
+company_location_city_list = company_location_city_list[:break_even]
+company_location_state_list = company_location_state_list[:break_even]
+salary_type_list = salary_type_list[:break_even]
+payment_cycle_list = payment_cycle_list[:break_even]
+lBound_list = lBound_list[:break_even]
+uBound_list = uBound_list[:break_even]
+currency_list = currency_list[:break_even]
+job_type_list = job_type_list[:break_even]
+benefits_list = benefits_list[:break_even]
+qualifications_list = qualifications_list[:break_even]
+job_description_list = job_description_list[:break_even]
+
+print(f"\nScraping complete! Sucessfully scraped {break_even} new jobs!")
 
 
 # Put all the collected data to a pandas dataframe
 
-df1 = pd.DataFrame({
+new_jobs_dataframe = pd.DataFrame({
     "job_title": job_title_list,
     "company_name": company_name_list,
     "rating": rating_list,
@@ -278,11 +356,20 @@ df1 = pd.DataFrame({
 
 })
 
-# Save the data to a csv file
+# Flip database setting top record to bottom record to maximize data integrity and accuracy timewise
+new_jobs_dataframe = new_jobs_dataframe.reindex(index=new_jobs_dataframe.index[::-1]).reset_index(drop=True)
 
-print("Saving fetched data to csv file (collected data.csv)")
+# Save the data to csv file
+# Updating the csv by joining the two fatareames
+
+updated_dataframe = pd.concat([existing_data_dataframe, new_jobs_dataframe], ignore_index=True)
+
+print("\n\nSaving fetched data to csv file (collected data.csv)")
 try:
-    tqdm(df1.to_csv("Collected data.csv"))
+    updated_dataframe.to_csv("Collected data.csv")
     print("Data saved successfully!!")
 except:
     print("Could not save data to csv file. Close any open instance of the csv and try again.")
+
+
+print("DONE!!")
